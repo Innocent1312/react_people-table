6057ae350e50ba65c3fa4fb7a07ab83db4063574
Innocent1312 <anthonydev33@gmail.com>
2020-01-02 13:02:35 +0200

    Updates

---
+++ precache-manifest.ca2cc7ab318005ea5b76436d0670b060.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3893f3691e6a5fdd6006cfca35659488",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "82e1b0332b14bc2caa89",
+    "url": "/react_people-table/static/css/main.75a27ff1.chunk.css"
+  },
+  {
+    "revision": "8bec324f9248f77333d7",
+    "url": "/react_people-table/static/js/2.65aa2fc1.chunk.js"
+  },
+  {
+    "revision": "82e1b0332b14bc2caa89",
+    "url": "/react_people-table/static/js/main.32429816.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
